4ba25b7df681d5c065b95ed575199fe31620595f
Brian crisp <bcrisp084@yahoo.com>
2021-02-09 21:01:37 -0500

    created package.JSON

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const fs = require("fs";)
-const inquirer = require("inquirer")
-const Engineer = require("./lib/engineer")
-const Engineer = require("./lib/intern")
-const Engineer = require("./lib/manager")
+const fs = require("fs");
+const inquirer = require("inquirer");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
 
-const employees = []
+const employees = [];
