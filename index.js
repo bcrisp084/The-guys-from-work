16f30a58b1a4f972e950991c22ebabb7057bba48
Brian crisp <bcrisp084@yahoo.com>
2021-02-09 20:52:04 -0500

    added classes to employee files

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const fs = require("fs";)
+const inquirer = require("inquirer")
+const Engineer = require("./lib/engineer")
+const Engineer = require("./lib/intern")
+const Engineer = require("./lib/manager")
+
+const employees = []
