525c332d814ca5af30262976344f5f4ec96e1fb1
Brian crisp <bcrisp084@yahoo.com>
2021-02-19 21:42:06 -0500

    created test for engineer

--- test/intern.test.js
+++ test/intern.test.js
@@ -1,1 +1,8 @@
+const { TestScheduler } = require("jest");
 const Intern = require("../lib/intern");
+
+Test("Can set school via constructor", () => {
+  const testThis = "Trilogy";
+  const e = new Intern("Jess", 9, "test@test", testThis);
+  expect(e.getSchool()).toBe(testThis);
+});
